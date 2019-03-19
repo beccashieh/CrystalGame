ece824370b6966c94effcdbd6691cd02b96a1454
BShieh <rebeccasshieh@gmail.com>
2019-03-19 11:56:51 -0400

    wip: working changes on crystal buttons.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
 //create global variables
 var wins = 0;
 var losses = 0;
-var buttons = (
+
+var buttons = {
     btn1 = Math.floor((Math.random() * 12) + 1),
     btn2 = Math.floor((Math.random() * 12) + 1),
     btn3 = Math.floor((Math.random() * 12) + 1),
     btn4 = Math.floor((Math.random() * 12) + 1),
-);
-var score;
+};
+var score= 0;
 var guess = Math.floor((Math.random() * 120) + 19);
 //reset function so game starts with new random value
 $(document).ready(function(){
@@ -21,6 +22,8 @@ $(document).ready(function(){
 
 //random number generator for each crystal button. (hidden)
 //I think an object could be used here? Not sure on the syntax...trying to attach a random value to each of the buttons indiv.
+var crystal = $("<img>");
+
 crystal.attr("data-crystalvalue", buttons);
 
     //on click, each number is added in the "score" variable.
@@ -30,6 +33,8 @@ crystal.attr("data-crystalvalue", buttons);
     // We then add the crystalValue to the user's "counter" which is a global variable.
     // Every click, from every crystal adds to the global counter.
         score += crystalValue;
+        document.getElementById("score").innerHTML = crystalValue;
+        console.log(crystalValue);
       });
 
 
